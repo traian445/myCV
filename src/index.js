afa21368333c965ff3be565f726453a3e9680ba7
cateatraian <traiangrigoriu78@gmail.com>
2025-03-16 17:44:03 +0200

    site

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+// Import our custom CSS
+import './scss/styles.scss'
+
+// Import https://github.com/mattboldt/typed.js/
+// import Typed from 'typed.js';
+import '../node_modules/typed.js/dist/typed.umd.js'
+
+import particles from 'particles.js'
+
+import './js/particles.js'
+
+
+import'./js/main.js'
